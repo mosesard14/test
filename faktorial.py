fe38fb0f321116395c32528cc1f6ad0fc4b4f9b6
moses puta <moses.putra@ti.ukdw.ac.id>
2025-02-23 20:57:17 +0700

    Create faktorial.py

---
+++ faktorial.py
@@ -0,0 +1,8 @@
+def faktorial(n):
+    hasil = 1
+    for i in range(1, n + 1):
+        hasil *= i
+    return hasil
+    
+bil = int(input("Masukkan bilangan :"))
+print(f"Hasil dari faktorial {bil} adalah = {faktorial(bil)}")
