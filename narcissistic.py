299acbd77e40925a50c9bd21c2187221dc84eb4c
moses puta <moses.putra@ti.ukdw.ac.id>
2025-02-25 00:03:02 +0700

    Create narcissistic.py

---
+++ narcissistic.py
@@ -0,0 +1,8 @@
+bilangan = (int(input("Masukkan bilangan :")))
+
+
+if bilangan > 0 and  bilangan < 10:
+    print(f"{bilangan} Narcissistic")
+elif bilangan :
+    puluhan = bilangan // 10
+    satuan = bilangan % 10
