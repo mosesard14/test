4210a942d0091af6849c7a6bf37b54468ca59c7e
moses puta <moses.putra@ti.ukdw.ac.id>
2025-03-22 00:04:21 +0700

    save

--- cekangka.py
+++ cekangka.py
@@ -3,6 +3,6 @@ def cek_angka(a, b, c):
         if a + b == c or a + c == b or b + c == a:
             return True
         elif a != b or a != c or b != c:
-            return True 
-    return False
-print(cek_angka(6345,3,6348))
+            return False
+    return True
+print(cek_angka(3,3,8))
---
+++ tes laprak.py
@@ -0,0 +1,6 @@
+umur = 19
+status = 'Pelajar'
+if umur > 18 and (status == 'Pelajar' or status == 'pekerja'):
+    print('anda seorang pekerja')
+else:
+    print(status)
